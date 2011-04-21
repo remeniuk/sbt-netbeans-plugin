c1d4766696a9231394e1c46d5308821cbbcb8602
Vasil Remeniuk <vasil.remeniuk@gmail.com>
2011-04-21 18:01:05 +0300

    Mixed-in posterous/implicit.ly plugin; added release notes

--- project/build/SbtNetbeansPluginProject.scala
+++ project/build/SbtNetbeansPluginProject.scala
@@ -1,7 +1,8 @@
 import sbt._
 import org.netbeans.plugins._
 
-class SbtNetbeansPluginProject(info: ProjectInfo) extends PluginProject(info) with SbtNetbeansPlugin{
+class SbtNetbeansPluginProject(info: ProjectInfo) extends PluginProject(info) 
+                                                     with SbtNetbeansPlugin with posterous.Publish{
 
   override def managedStyle = ManagedStyle.Maven
   lazy val publishTo = Resolver.file("GitHub", new java.io.File("../remeniuk.github.com/maven/"))
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -8,5 +8,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
 
   val netbeansPluginRepo = "Netbeans Plugin Github Repo" at "http://remeniuk.github.com/maven/"
   val netbeansPlugin = "org.netbeans.plugin" % "sbt-netbeans-plugin" % "0.0.1"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.7"
 
 }
