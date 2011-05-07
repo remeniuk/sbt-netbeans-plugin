11baa4b4fc9e83d657a7480c5f10e8f535e021f9
Vasil Remeniuk <vasil.remeniuk@gmail.com>
2011-05-07 17:05:54 +0300

    Netbeans files (classpath settings) are updated every time SBT project classpath changes

--- src/main/scala/netbeans/NetbeansCommands.scala
+++ src/main/scala/netbeans/NetbeansCommands.scala
@@ -14,21 +14,26 @@ object NetbeansCommands {
   import scalaz._
   import Scalaz._
     
-  private val create = createNetbeansFiles _ *> updateAll _
-  private val createSourceDirs = createSourceDirectories _ *> updateProjectConfig _
+  val create = for{c <- createNetbeansFiles(_); 
+                   u <- updateAll(_)} yield c andThen u
   
+  val createSourceDirs = for{c <- createSourceDirectories(_); 
+                             u <- updateProjectConfig(_)} yield c andThen u
+  
+  /** If `transitive` is used, command is applied to all projects used by 
+   * current project  */
   val transitive = token(Space ~> ("transitive" ^^^ true)) ?? false
   
   val createOptions = "create" ~> (token(Space ~> ("source-directories" ^^^ createSourceDirs)) ?? create)
   
-  val updateOptions = "update" ~> token(Space ~> ("all" ^^^ updateAll _ | 
-                                                  "dependencies" ^^^ updateProjectProperties _)) 
+  val updateOptions = "update" ~> (token(Space ~> ("all" ^^^ updateAll _ | 
+                                                   "dependencies" ^^^ updateProjectProperties _)) ?? updateAll _) 
       
   val removeOptions = "remove" ^^^ removeNetbeansFiles _
   
   val netbeansConsole = Space  ~> 
   (((createOptions | updateOptions | removeOptions) ~ transitive) map {      
       case (command, transitive) => (command, transitive)
-    }) ?? (create, false)
+    }) ?? (create, true)
 
 }
--- src/main/scala/netbeans/NetbeansPlugin.scala
+++ src/main/scala/netbeans/NetbeansPlugin.scala
@@ -7,17 +7,22 @@ import CommandSupport._
 
 object NetbeansPlugin extends Plugin {
   
+  /** build.xml template under ./sbt/plugins (overrides the template in plugin jar) */
   private val buildXmlTemplateLocation = BuildPaths.defaultGlobalPlugins / "src" / "main" / "resources" / "build.xml"
+  /** project.properties template under ./sbt/plugins (overrides the template in plugin jar) */
   private val projectFilesTemplateLocation = BuildPaths.defaultGlobalPlugins / "src" / "main" / "resources" / "nbproject"
   
+  /** Extracts Netbeans project files templates from the plugin jar */
   private def copyNetbeansFiles(basePath: Path)(pluginJarPath: File) = 
     IO.unzip(pluginJarPath, (basePath / "test").asFile, "*.xml" | "*.properties") 
   
+  /** Copies packed Netbeans project files templates to the project folder */
   private def copyPackedTemplates(libClasspath: Classpath, dest: Path) = {
     libClasspath.filter(_.data.getName.contains("sbt-netbeans-plugin"))      
     .headOption.map(_.data).map(copyNetbeansFiles(dest))                  
   }  
     
+  /** Copies Netbeans project files templates from ./sbt/plugins */
   private def copyUnpackedTemplates(dest: Path) = {
     buildXmlTemplateLocation.get.map { template =>
       IO.copy(Seq(template.asFile -> (dest / "build.xml").asFile), false)  
@@ -26,12 +31,14 @@ object NetbeansPlugin extends Plugin {
       IO.copyDirectory(template.asFile, (dest / "nbproject").asFile, false)  
     }    
   }
-      
+  
+  /** Adds sbt-netbeans commands globally */
   override lazy val settings = Seq(commands += netbeansCommands)
   
+  /** Updates Netbeans project files with SBT project settings */
   private[netbeans] def updateNetbeansFiles(projectRef: ProjectRef, 
                                             s: State, 
-                                            projectFiles: Seq[ProjectContext => NetbeansConfigFile]) = {
+                                            projectFiles: Seq[ProjectContext => NetbeansConfigFile]): State = {
 
     import scalaz.Scalaz._
     
@@ -49,23 +56,29 @@ object NetbeansPlugin extends Plugin {
           }        
         )
       }
-    }    
+    } 
+    
+    s
   }
-
-  private[netbeans] def updateAll(projectRef: ProjectRef, s: State) = 
+        
+  /** Updates all Netbeans project files */
+  private[netbeans] def updateAll(projectRef: ProjectRef)(s: State): State = 
     updateNetbeansFiles(projectRef, s, 
                         Seq((context => AntScript(context.baseDirectory / "build.xml")(context)), 
                             (context => ProjectConfiguration(context.baseDirectory / "nbproject" /"project.xml")(context)),
                             (context => ProjectProperties(context.baseDirectory / "nbproject" / "project.properties")(context)))
     )
-  
-  private[netbeans] def updateProjectProperties(projectRef: ProjectRef, s: State) = 
-    updateNetbeansFiles(projectRef, s, Seq((context => ProjectConfiguration(context.baseDirectory / "nbproject" /"project.xml")(context))))
+    
+  /** Updates project.properties */
+  private[netbeans] def updateProjectProperties(projectRef: ProjectRef)(s: State): State = 
+    updateNetbeansFiles(projectRef, s, Seq((context => ProjectProperties(context.baseDirectory / "nbproject" /"project.properties")(context))))
 
-  private[netbeans] def updateProjectConfig(projectRef: ProjectRef, s: State) = 
+  /** Updates project.xml */
+  private[netbeans] def updateProjectConfig(projectRef: ProjectRef)(s: State): State = 
     updateNetbeansFiles(projectRef, s, Seq((context => ProjectConfiguration(context.baseDirectory / "nbproject" /"project.xml")(context))))
   
-  private[netbeans] def createSourceDirectories(projectRef: ProjectRef, s: State) =         
+  /** Creates empty source/resource directories */
+  private[netbeans] def createSourceDirectories(projectRef: ProjectRef)(s: State): State = {        
     ProjectContext.netbeansContext(projectRef, s).map{context =>
       logger(s).info("Creating empty source directories for project `%s`..." format(projectRef.project))
       
@@ -74,10 +87,13 @@ object NetbeansPlugin extends Plugin {
           context.baseDirectory / "src" / "test" / "scala",
           context.baseDirectory / "src" / "main" / "resources",
           context.baseDirectory / "src" / "test" / "resources"
-        ))
-    } 
-    
-  private[netbeans] def createNetbeansFiles(projectRef: ProjectRef, s: State) = {
+        ))            
+    }
+    s
+  } 
+   
+  /** Adds Netbeans project files to the SBT project */
+  private[netbeans] def createNetbeansFiles(projectRef: ProjectRef)(s: State): State = {
     val extracted = Project extract s
     import extracted._
     
@@ -87,9 +103,16 @@ object NetbeansPlugin extends Plugin {
       ProjectContext.toOption(classpath).map(copyPackedTemplates(_, base))
       copyUnpackedTemplates(base)
     }
+    
+    if(session.original.filter(_.key.key.label == NetbeansTasks.updateDepTaskKey).isEmpty){
+      logger(s).info("Writing plugin settings for project `%s`" format(projectRef.project))
+      NetbeansTasks.writePluginSettings(projectRef, structure)    
+      s.reload
+    } else s
   }
   
-  private[netbeans] def removeNetbeansFiles(projectRef: ProjectRef, s: State) = {
+  /** Removes Netbeans project files from the SBT project */
+  private[netbeans] def removeNetbeansFiles(projectRef: ProjectRef)(s: State): State = {
     val extracted = Project extract s
     import extracted._
     
@@ -97,23 +120,24 @@ object NetbeansPlugin extends Plugin {
     (baseDirectory in (projectRef, Compile) get structure.data) map { base =>
       IO.delete((base / "build.xml" +++ base / "nbproject").get)
     }
+    
+    s
   }
     
-  type NetbeansCommand = (ProjectRef, State) => Any
-  
+  type NetbeansCommand = ProjectRef => State => State
+    
   lazy val netbeansCommands = 
     Command("netbeans")(_ => NetbeansCommands.netbeansConsole) { (state: State, output: Any) =>
       output match {
         case (cmd: NetbeansCommand, transitive: Boolean) => 
           val extracted = Project extract state
-          cmd(extracted.currentRef, state)
+          val s = cmd(extracted.currentRef)(state)
           if(transitive){
             logger(state).info("Executing command transitively...")
-            extracted.currentProject.uses.foreach(cmd(_, state))
-          }
-          state
-        case _ =>  
-          logger(state).error("Failed to process command line!")
+            (s /: extracted.currentProject.uses) {(_s, _ref) => cmd(_ref)(_s)}
+          } else s
+        case other =>  
+          logger(state).error("Failed to process command line: " + other)
           state.fail   
       }
     }
